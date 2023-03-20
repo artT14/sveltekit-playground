003a1b54c17c91a70f473ace503139d05a8b1fa5
artT14 <lamferme@gmail.com>
2023-03-19 23:41:40 -0700

    optional parameters

---
+++ hello-world/src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {page} from "$app/stores";
+    const greetings = {
+        en: "Hello!",
+        es: "¡Hola!",
+        fr: "Bonjour!"
+    };
+    const {lang = "en" } = $page.params; //'en' by default
+    const greeting = greetings[lang];
+</script>
+
+<h1>{greeting}</h1>
