17347e7ed14019bfcc1fa5c72097fbebb4cb9103
artT14 <lamferme@gmail.com>
2023-03-19 23:28:14 -0700

    routing

---
+++ hello-world/src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import {page} from "$app/stores";
+    const slugArr = $page.params.slug.split('/')
+</script>
+
+{#if slugArr.length === 1}
+<h1>Viewing Docs for feature {slugArr[0]}</h1>
+{:else if slugArr.length === 2}
+<h1>Viewing Docs for feature {slugArr[0]} and concept {slugArr[1]}</h1>
+{/if}
---
+++ hello-world/src/routes/products/[productId]/reviews/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {page} from '$app/stores'
+    const productId = $page.params.productId
+</script>
+
+<h1>Reviews About Product {productId}</h1>
