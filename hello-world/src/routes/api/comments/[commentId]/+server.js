8669fb76a62c21a4751136eb576aac989402c3c3
artT14 <lamferme@gmail.com>
2023-03-26 02:36:49 -0700

    DELETE API Routes

--- hello-world/src/routes/api/comments/[commentId]/+server.js
+++ hello-world/src/routes/api/comments/[commentId]/+server.js
@@ -20,3 +20,16 @@ export async function PATCH(requestEvent){
     comment.text = text
     return json(comment)
 }
+
+export async function DELETE(requestEvent){
+    const {params} = requestEvent;
+    const {commentId} = params;
+    const deletedComment = comments.find(
+        (comment) => comment.id === parseInt(commentId)
+    );
+    const commentIndex = comments.findIndex(
+        (comment) => comment.id === parseInt(commentId)
+    );
+    comments.splice(commentIndex,1);
+    return json(deletedComment);
+}
