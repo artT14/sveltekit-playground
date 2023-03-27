c46c5953b043c57086bbcfb59f80025825b0d031
artT14 <lamferme@gmail.com>
2023-03-26 19:58:16 -0700

    Errors and Redirects

--- sk-loading-data/src/routes/products/[productId]/+page.server.js
+++ sk-loading-data/src/routes/products/[productId]/+page.server.js
@@ -1,8 +1,17 @@
+import { error, redirect } from '@sveltejs/kit'
+
 // SERVER LOAD FUNCTION
 export const load = async (serverLoadEvent) =>{
     const { fetch, params, url, route } = serverLoadEvent;
     console.log({params, url, route: route.id})
     const {productId} = params;
+    if(productId > 3){
+        throw error(404,{
+            message: 'Oh no! Looks like the product is currently unavailable.',
+            hint: 'Try a different product.'
+        });
+        // throw redirect(307, '/products');
+    }
     const title = "Product Details";
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     const product = await response.json();
