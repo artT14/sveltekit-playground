3199e8109367576f97d94562512fc4bae8129c8f
artT14 <lamferme@gmail.com>
2023-03-26 20:32:47 -0700

    Layout Data

---
+++ sk-loading-data/src/routes/products/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let data;
+    const {title, featuredProducts} = data;
+</script>
+
+<slot />
+<h2>{title}</h2>
+{#each featuredProducts as product}
+    <div class="product-item">
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
+    </div>
+{/each}
+
+<style>
+    .product-item{
+        display: inline-block;
+        width: 200px;
+        margin: 10px;
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+</style>
