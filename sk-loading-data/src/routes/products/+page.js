99f06b4c518e2e8c16a4dff12824ad4f1aa4cdd9
artT14 <lamferme@gmail.com>
2023-03-26 18:38:54 -0700

    Universal Load Function + Server Load Function

--- sk-loading-data/src/routes/products/+page.js
+++ sk-loading-data/src/routes/products/+page.js
@@ -1,10 +1,13 @@
-export const load = async (loadEvent) =>{
-    const { fetch } = loadEvent;
-    const title = "List of available products";
-    const response = await fetch("http://localhost:4000/products/");
-    const products = await response.json();
-    return {
-        title,
-        products
-    }
-}
+// UNIVERSAL LOAD FUNCTION
+
+// export const load = async (loadEvent) =>{
+//     console.log("Load func called in +page.js")
+//     const { fetch } = loadEvent;
+//     const title = "List of available products";
+//     const response = await fetch("http://localhost:4000/products/");
+//     const products = await response.json();
+//     return {
+//         title,
+//         products
+//     }
+// }
