89eb3e08562cb3de0c60608ac3d4883b75aa78cb
artT14 <lamferme@gmail.com>
2023-03-26 18:18:16 -0700

    Loading Data; Page Data

---
+++ sk-loading-data/src/routes/products/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data;
+    const {title, products} = data
+</script>
+
+<h1>{title}</h1>
+{#each products as product}
+<div>
+    <h2>{product.title}</h2>
+    <p>{product.description}</p>
+    <hr/>
+</div>
+{/each}
