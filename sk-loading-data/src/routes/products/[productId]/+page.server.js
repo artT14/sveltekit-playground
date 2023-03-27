b7b73b721de0df28abc9a89ab6437fc8a56da15f
artT14 <lamferme@gmail.com>
2023-03-26 19:37:34 -0700

    Using URL Data

---
+++ sk-loading-data/src/routes/products/[productId]/+page.server.js
@@ -0,0 +1,13 @@
+// SERVER LOAD FUNCTION
+export const load = async (serverLoadEvent) =>{
+    const { fetch, params, url, route } = serverLoadEvent;
+    console.log({params, url, route: route.id})
+    const {productId} = params;
+    const title = "Product Details";
+    const response = await fetch(`http://localhost:4000/products/${productId}`);
+    const product = await response.json();
+    return {
+        title,
+        product
+    }
+}
