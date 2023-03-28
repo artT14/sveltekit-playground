0f64a8f3b0ea2fe4fd4196da1afadc1c25122370
artT14 <lamferme@gmail.com>
2023-03-27 20:00:12 -0700

    Default Form Actions

---
+++ sk-form-actions/src/routes/auth/+page.server.js
@@ -0,0 +1,12 @@
+export const actions = {
+    default: async ({request, cookies})=>{
+        const data = await request.formData();
+        const username = data.get('username');
+        const password = data.get('password');
+        if (!username || !password){
+            return {message: 'Missing Username or Password'}
+        }
+        cookies.set('username', username, {path: '/'});
+        return {message: 'Logged In'};
+    }
+}
